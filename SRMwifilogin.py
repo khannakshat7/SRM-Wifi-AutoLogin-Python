c7d5c54a41185de00930942b80a381a64ce7361f
Akshat Khanna <khannakshat7@gmail.com>
2019-08-24 15:37:21 +0530

    Wifi Login

---
+++ SRMwifilogin.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+username = '' # Enter Your SRM Wifi Username 
+password = '' # Enter Your SRM Wifi password
+
+url = 'https://iac.srmist.edu.in/connect/PortalMain'  # SRM Wifi Login URL
+
+driver = webdriver.Chrome("chromedriver_win32\\chromedriver.exe")  # Link to Chrome Driver
+
+driver.get(url)
+time.sleep(0.2)
+try:
+    driver.find_element_by_id('LoginUserPassword_auth_username').send_keys(username)
+    driver.find_element_by_id('LoginUserPassword_auth_password').send_keys(password)
+    driver.find_element_by_id('UserCheck_Login_Button').click()
+except:
+    driver.refresh()
+    driver.find_element_by_id('LoginUserPassword_auth_username').send_keys(username)
+    driver.find_element_by_id('LoginUserPassword_auth_password').send_keys(password)
+    driver.find_element_by_id('UserCheck_Login_Button').click()
